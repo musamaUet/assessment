e8515faee9bfd094ba54c9fa8f6f84305eee3455
musamauet <usamaitians.gcuf@gmail.com>
2025-01-23 22:49:16 +0500

    dockerize backend and fornten

---
+++ realtime-bidding-frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/TS/JSX/TSX files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
