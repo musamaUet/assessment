91c0e2eaac16bbdf291d35d54b47b6c7bece1a15
musamauet <usamaitians.gcuf@gmail.com>
2025-01-29 09:15:18 +0500

    update backend

--- realtime-bidding-backend/src/users/user.module.ts
+++ realtime-bidding-backend/src/users/user.module.ts
@@ -3,10 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
+import { JwtAuthGuard } from './auth.guard';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default_secret', // Use environment variable for security
+      signOptions: { expiresIn: '1h' }, // Token expires in 1 hour
+    }),
+  ],
+  providers: [UserService, JwtAuthGuard],
   controllers: [UserController],
+  exports: [UserService],
 })
 export class UsersModule {}
